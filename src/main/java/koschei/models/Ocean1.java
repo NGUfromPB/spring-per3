93ec08028c3490c1ba006c3a1ecf2fb6647d236b
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-02-24 12:09:04 +0300

    Add files via upload

---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+    private final Island2 island2;
+
+    public Ocean1(Island2 island2) {
+        this.island2 = island2;
+    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island2.toString();
+    }
+}
